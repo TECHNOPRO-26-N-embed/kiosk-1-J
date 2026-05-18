dc576160123c85e5d57d33a90d1bcafaa52ab346
Cuong-Pham-TP <282411312+Cuong-Pham-TP@users.noreply.github.com>
2026-05-18 16:48:13 +0900

    add new

--- src/main.c
+++ src/main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 #include <time.h>   
+
+char user[][3][50] = {
+    {"B0101", "ファム チュンクオン","090-1111-2222"},
+    {"B0102", "高木 歩夢","090-2222-3333"},
+    {"B0103", "瀬戸 雅也","090-3333-4444"},
+    {"B0104", "張 芸超","090-4444-5555"},
+    {"B0105", "キム ミンギュ","090-5555-6666"}
+};
+
+void check_user_id(char user_id[]) {
+    //ユーザーID確認画面
+    printf("ユーザーIDを入力してください\n");
+    //ユーザーID取得
+    scanf("%s",user_id);
+
+    for(int i = 0; i < 5; i++){
+        if(strcmp(user_id, user[i][0]) == 0){
+            printf("ユーザーIDが確認されました。ようこそ、%sさん！\n", user[i][1]);
+            return ;
+        }
+    }
+    printf("ユーザーIDが見つかりませんでした。もう一度入力してください。\n");
+    strcpy(user_id, ""); // ユーザーIDを空にして再入力を促す
+}
 
 int calculate(char due_date[], char return_date[]) {
     // 返却予定日と返却日の差を計算する処理をここに実装
@@ -86,6 +111,11 @@ int main() {
     char due_date[20];
     char return_date[20];
 
+    check_user_id(user_id);
+
+    if(strcmp(user_id, "") == 0) {
+        return 0; // ユーザーIDが確認されなかった場合、プログラムを終了
+    }
 
     while (1){
         printf("\n==============================\n");
